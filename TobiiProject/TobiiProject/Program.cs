3ec6893f21d31158a0b7b8ff42fc6a40b0133fef
Kevin Meister <kevin.meister@students.unibe.ch>
2018-05-23 17:39:55 +0200

    implemented set cursor position and change active window with gaze

--- TobiiProject/TobiiProject/Program.cs
+++ TobiiProject/TobiiProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Gma.System.MouseKeyHook;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
@@ -17,15 +18,37 @@ namespace TobiiProject
         [STAThread]
         static void Main()
         {
-
+            int xCord = 0;
+            int yCord = 0;
             
             var host = new Host();
             var gazePointDataStream = host.Streams.CreateGazePointDataStream();
 
+            gazePointDataStream.GazePoint((x, y, ts) => { xCord = (int)x; yCord = (int)y; });
             Hook.GlobalEvents().OnCombination(new Dictionary<Combination, Action>
                 {
-                    {Combination.FromString("Shift+Alt+Enter"), () => { gazePointDataStream.GazePoint((x, y, ts) => Console.WriteLine("Timestamp: {0}\t X: {1} Y:{2}", ts, x, y)); }}
-                });
+                    {Combination.FromString("Shift+Alt+Enter"), () => { gazePointDataStream.GazePoint((x, y, ts) => 
+                            SetCursorPos(
+                                  (int)x,
+                                  (int)y
+                                ));
+                        }
+                    },
+                    {Combination.FromString("Shift+Alt+I"), () => {
+                            SetCursorPos(xCord,yCord);
+                        }
+                    },
+                    {Combination.FromString("Shift+Alt+F"), () => {
+                            SetForegroundWindow(WindowFromPoint(new Point(xCord,yCord)));
+                        }
+                    }
+
+
+        });
+            /*SetCursorPos(
+                          1300,
+                          700
+                        );*/
 
             // launch the WinForms application like normal
             Application.EnableVisualStyles();
@@ -33,5 +56,14 @@ namespace TobiiProject
             Application.Run(new Form1());
 
         }
+
+        [DllImport("user32")]
+        public static extern IntPtr WindowFromPoint(Point point);
+
+        [DllImport("user32")]
+        public static extern bool SetForegroundWindow(IntPtr process);
+
+        [DllImport("user32")]
+        public static extern bool SetCursorPos(int x, int y);
     }
 }
