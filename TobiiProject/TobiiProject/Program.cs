0c32c1bf4ff983fcc86110aa6f29cb483d4f48f0
Kevin Meister <kevin.meister@students.unibe.ch>
2018-05-23 16:34:06 +0200

    first steps

---
+++ TobiiProject/TobiiProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tobii.Interaction;
+
+namespace TobiiProject
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            
+            var host = new Host();
+            var gazePointDataStream = host.Streams.CreateGazePointDataStream();
+
+            Hook.GlobalEvents().OnCombination(new Dictionary<Combination, Action>
+                {
+                    {Combination.FromString("Shift+Alt+Enter"), () => { gazePointDataStream.GazePoint((x, y, ts) => Console.WriteLine("Timestamp: {0}\t X: {1} Y:{2}", ts, x, y)); }}
+                });
+
+            // launch the WinForms application like normal
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Console.WriteLine("shit it works");
+            MessageBox.Show("test");
+
+            Hook.GlobalEvents().OnCombination(new Dictionary<Combination, Action>
+{
+    {Combination.FromString("Shift+Alt+Enter"), () => { Console.WriteLine("You Pressed FULL SCREEN"); }}
+});
+        }
+    }
+}
